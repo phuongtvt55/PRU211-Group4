6d82aa25b894b43d6d3e2aa014ccba3fc21ac23a
HuyNQ2002 <huynqce161074@fpt.edu.vn>
2023-06-27 13:26:10 +0700

    pushed lan 2

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogueScript;
+    private bool playerDetected;
+    private Animator anim;
+
+    //Detect trigger with player
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If we triggerd the player enable playerdeteced and show indicator
+        if(collision.tag == "Player")
+        {
+            playerDetected = true;
+            dialogueScript.ToggleIndicator(playerDetected);
+            anim.SetBool("isStanding", true);
+            anim.SetBool("isWalking", false);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //If we lost trigger  with the player disable playerdeteced and hide indicator
+        if (collision.tag == "Player")
+        {
+            playerDetected = false;
+            dialogueScript.ToggleIndicator(playerDetected);
+            dialogueScript.EndDialogue();
+            anim.SetBool("isStanding", false);
+            anim.SetBool("isWalking", true);
+        }
+    }
+    //While detected if we interact start the dialogue
+    private void Update()
+    {
+        if(playerDetected && Input.GetKeyDown(KeyCode.E))
+        {
+            dialogueScript.StartDialogue();
+        }
+    }
+}
--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class beardedPatrol : MonoBehaviour
+public class Patrol : MonoBehaviour
 {
     public GameObject pointA;
     public GameObject pointB;
