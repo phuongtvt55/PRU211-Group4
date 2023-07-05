faebfee01b9249b6ba7f3f7ced2ae61c28ed1b63
phuongtvt55 <phuongtvt55@gmail.com>
2023-07-05 18:13:07 +0700

    update

--- Assets/Scripts/StateMachine/Boss_behaviour.cs
+++ Assets/Scripts/StateMachine/Boss_behaviour.cs
@@ -8,9 +8,7 @@ public class Boss_behaviour : StateMachineBehaviour
     private Rigidbody2D rb;
     [SerializeField]
     private float movement;
-    [SerializeField]
-    private GameObject knight;
-
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
