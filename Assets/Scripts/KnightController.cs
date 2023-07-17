345e939e62ad0e84ee1c67d54d1816d65149734b
phuongtvt55 <phuongtvt55@gmail.com>
2023-07-18 01:24:14 +0700

    update final

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -1,7 +1,6 @@
 using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections))]
